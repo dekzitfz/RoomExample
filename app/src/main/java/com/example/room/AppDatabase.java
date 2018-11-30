ccf70abca78abd78bd15b0e3e8a9456fe763d91b
Adi Andrea <dekz.itfz@gmail.com>
2018-11-30 15:03:54 +0800

    setup instance for AppDatabase

--- app/src/main/java/com/example/room/AppDatabase.java
+++ app/src/main/java/com/example/room/AppDatabase.java
@@ -1,7 +1,9 @@
 package com.example.room;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 import com.example.room.dao.MahasiswaDao;
 import com.example.room.entity.Mahasiswa;
@@ -9,6 +11,24 @@ import com.example.room.entity.Mahasiswa;
 @Database(entities = {Mahasiswa.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 
+    private static AppDatabase INSTANCE;
+
     public abstract MahasiswaDao mahasiswaDao();
+
+    public static AppDatabase getAppDatabase(Context context){
+        if(INSTANCE == null){
+            INSTANCE = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    AppDatabase.class,
+                    "database_mahasiswa"
+            ).allowMainThreadQueries().build();
+        }
+
+        return INSTANCE;
+    }
+
+    public static void destroyInstance(){
+        INSTANCE = null;
+    }
 
 }
